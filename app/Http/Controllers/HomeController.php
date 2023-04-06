1575f8e7ba48af7a4859f561293775fab7e27bc7
Jan Rodney Costes <jrmcostes@gmail.com>
2023-04-06 16:45:20 +0800

    changeee

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\C1answers;
+use Barryvdh\Snappy\Facades\SnappyPdf as PDF;
 
 use Illuminate\Http\Request;
 
@@ -25,5 +26,23 @@ class HomeController extends Controller
     public function index()
     {
         return view('index');
+    }
+    public function pdf_print(){
+        $id = $_GET['formid'];
+
+        $form = C1answers::where('id', $id)->first();
+
+        $answersc1 = json_decode($form->c1answers, true);
+        $answersc2 = json_decode($form->c2answers, true);
+        $answersc3 = json_decode($form->c3answers, true);
+        $answersc4 = json_decode($form->c4answers, true);
+        $surname = $form->surname;
+        $firstname = $form->surname;
+        $sex = $form->sex;
+
+        $pdf = App::make('snappy.pdf.wrapper');
+        $pdf = PDF::loadvIEW('pdf.pdftemplate', compact('answersc1', 'answersc2', 'answersc3', 'answersc4', 'firstname', 'surname', 'sex'));
+
+        return $pdf->stream('pdsform.pdf');
     }
 }
