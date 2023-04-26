6ee1a76b695e7c399ca7cc9ec51572e44128f45d
earthtosky <109052932+earthtosky@users.noreply.github.com>
2023-04-26 12:47:41 +0800

    ui changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -129,7 +129,7 @@
             <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h5 class="modal-title">Reminder</h5>
+                  <h4 class="modal-title">Reminder</h4>
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
@@ -140,7 +140,7 @@
 
                         <p>
                            <br> By continuing to browse this website, you agree to DOST Region XI’ Data Privacy Policy. The full text of the Policy can be accessed through this
-                         <i> <a href="https://www.sei.dost.gov.ph/index.php/about-dost-sei/data-privacy-notice#:~:text=The%20DOST-SEI%2C%20in%20compliance,access%20to%20the%20collected%20information" target="_blank" style="text-decoration: none; color: blue; text-align: center;">link</a></i>
+                         <i> <a href="https://www.sei.dost.gov.ph/index.php/about-dost-sei/data-privacy-notice#:~:text=The%20DOST-SEI%2C%20in%20compliance,access%20to%20the%20collected%20information" target="_blank" style="text-decoration: none; color: blue; text-align: center;">link.</a></i>
                         <br>
                         </p>
                 </div>
